302acd188e065afc55c2c8aa61450f5f488e2b45
audrey157 <146869707+audrey157@users.noreply.github.com>
2024-10-13 02:22:07 -0700

    very basic layout

--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -3,19 +3,27 @@ import React from 'react';
 function NavBar() {
     return (
         <>
-        <div className="bg-emerald-700 h-full w-full">
-            <p>should be a nav bar</p>
+        <div className="bg-emerald-700 h-full w-full py-6">
+            <p className="text-center font-mono text-white">pickle ai</p>
         </div>
         </> 
     );
 }
 
 function Background() {
-    
+    return (
+        <div className="flex justify-evenly">
+            <div className="bg-emerald-600 h-screen w-1/4"></div>
+            <div className="bg-slate-300 h-screen w-3/4"><p>code environment</p></div>
+        </div>
+    )
 }
 
 export default function WorkSpace() {
     return (
+        <>
         <NavBar></NavBar>
+        <Background></Background>
+        </>
     )
 }
